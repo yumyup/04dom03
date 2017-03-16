668b7a658ee4cd902d7bf7108091d225c8fcefac
yupi <grafik@10g.pl>
2017-03-16 16:37:26 +0100

    stworzenie programu

---
+++ src/Num.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Helmut on 2017-03-15.
+ */
+public class Num {
+    public static void main(String[] args) {
+        double i = 0.0;
+        double sum = i + 0.1;
+        while (i < 3.1) {
+            System.out.printf("liczba %.2f\n ", i);
+            i = i+0.1;
+        }
+
+
+
+
+    }
+}
